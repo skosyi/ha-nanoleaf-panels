6b399429b5d2169c67894ac00137357793ffb83a
Sergei Kosyi <sergey.kosy@gmail.com>
2023-03-12 23:53:47 -0700

    update.sh

--- update.sh
+++ update.sh
@@ -6,15 +6,13 @@ echo current branch: $current_branch
 src_repo="https://github.com/skosyi/ha-core"
 src_branch="nanoleaf_panels"
 src_folder="homeassistant/components/nanoleaf_panels"
+src_repo_alias="ha_core"
 dst_folder="custom_components/nanoleaf_panels"
 
-# check out the src repo into a new branch
-git remote add src_repo $src_repo
-git fetch src_repo
-git branch src_repo_branch src_repo/$src_branch
-
-# split desired folder into a branch
-git checkout -f src_repo_branch
+# check out the src 
+git remote add src_repo_alias $src_repo
+git fetch src_repo_alias
+git checkout -f src_repo_alias/$src_branch
 git subtree split --prefix=$src_folder -b split_folder_branch
 
 # check out target branch and add all commits in temp branch to the correct folder
@@ -23,5 +21,4 @@ git subtree add --prefix=$dst_folder split_folder_branch
 
 # clean up
 git branch -d split_folder_branch
-git branch -d src_repo_branch
-git remote remove src_repo
+git remote remove src_repo_alias
